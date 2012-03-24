3ff70cfb28812e0472a79fa35d0f66f92f21392a
Mads Hartmann <mads379@gmail.com>
2012-03-25 00:46:33 +0100

    Using Bootstrap for the site

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -4,6 +4,7 @@ import net.liftweb._
 import http.{LiftRules, NotFoundAsTemplate, ParsePath}
 import sitemap.{SiteMap, Menu, Loc}
 import util.{ NamedPF }
+import net.liftweb.http.{ Req, Html5Properties }
 
 
 
@@ -14,19 +15,23 @@ class Boot {
     LiftRules.addToPackages("com.sidewayscoding")
 
     // build sitemap
-    val entries = (List(Menu("Home") / "index") ::: Nil)
-    
+    val entries = List(
+      Menu("Home") / "index",
+      Menu("Example") / "example")
+
     LiftRules.uriNotFound.prepend(NamedPF("404handler"){
       case (req,failure) => NotFoundAsTemplate(
         ParsePath(List("exceptions","404"),"html",false,false))
     })
-    
+
     LiftRules.setSiteMap(SiteMap(entries:_*))
-    
+
     // set character encoding
     LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
-    
-    
-    
+
+    // HTML5 Rendering
+    LiftRules.htmlProperties.default.set((r: Req) =>
+      new Html5Properties(r.userAgent))
+
   }
 }
