164d2d822f23017ab03aa06f89d979e9ce97f7c4
Mads Hartmann <mads379@gmail.com>
2012-03-24 23:41:51 +0100

    Now with a way to deal with failures.
    The function is now Env => Either[Error, A]. This makes it possible
    to shows errors. I'm using a fail-fast approach right now.

--- src/main/scala/com/sidewayscoding/formlets/Formlet.scala
+++ src/main/scala/com/sidewayscoding/formlets/Formlet.scala
@@ -14,11 +14,16 @@
     - We don't have about name sources as Lift has built-in
       functionality for generating unique names for fields; This
       has the advantage of also being more safe against hackers etc.
+
     - We need to keep track of all of the names used so we can construct
       the enviornment to pass to the function upon form submit.
 
+    - We want to deal with possible failures; either because someone has
+      messed with the markup or because some user defined validation
+      fails.
+
   So rather than being a Int => (NodeSeq, Env => A, Int) we now have
-  (NodeSeq, Env => A, Names).
+  (NodeSeq, Env => Either[Error, A], Names).
 
 */
 
@@ -30,18 +35,34 @@ import net.liftweb.util.Helpers.{ nextFuncName }
 import net.liftweb.http.{ S }
 import net.liftweb.common.{ Box }
 
+/**
+ * TODO: Write documentation
+ */
 trait Formlet[A] {
 
-  import Formlet.{ Env, Names }
+  import Formlet.{ Env, Names, Error }
 
-  val value: (NodeSeq, Env => A, Names)
+  val value: (NodeSeq, Env => Either[Error, A], Names)
 
+  /**
+   * TODO: Write documentation
+   */
   def <*>[X,Y](x: Formlet[X])(implicit ev: A <:< (X => Y)) = {
-    val (xml, func, names) = this.value
+
+    val (xml,  func,  names)  = this.value
     val (xml2, func2, names2) = x.value
+
+    // Fail fast - btw, where are the flatMap/map methods in Either? :-(
+    val f = (env: Env) => func2(env).fold(
+      (fail) => Left(fail),
+      (success) => func(env).fold(
+        (fail) => Left(fail),
+        (g) => Right(g(success))
+      )
+    )
+
     new Formlet[Y] {
-      val value =
-        (xml ++ xml2, ( (env: Env) => func(env)(func2(env) ) ), names ::: names2)
+      val value = (xml ++ xml2, f, names ::: names2)
     }
   }
 
@@ -54,7 +75,7 @@ trait Formlet[A] {
   }
 
   /**
-   *
+   * TODO: Write documentation
    */
   def form: NodeSeq = {
 
@@ -69,35 +90,41 @@ trait Formlet[A] {
 
       val empty = Map[String,String]()
 
-      val env = (names zip (names map { S.param _ })).foldLeft (empty) {
+      val mapping = names zip ( names map { S.param _ } )
+
+      val env = mapping.foldLeft (empty) {
         case (map: Env, (key: String, box: Box[String])) =>
           box map { value => map + (key -> value) } openOr map
       }
 
-      func(env)
+      val result = func(env)
+
+      if (result.isLeft) {
+        S.error(result.left.get)
+      }
 
     }
 
     // Tell lift about the form handler
     S.addFunctionMap(name, collector)
 
-    <form>
-      {
-        html ++ <input type="submit" name={{name}} value="Submit" />
-      }
-    </form>
+    <form> { html ++ <input type="submit" name={{name}} value="Submit" /> } </form>
   }
 
 }
 
+/**
+ * TODO: Write documentation
+ */
 object Formlet {
 
   type Env = Map[String, String]
   type Names = List[String]
+  type Error = String
 
   def apply[A](a: A): Formlet[A] = {
     new Formlet[A] {
-       val value = (NodeSeq.Empty, (_: Env) => a, Nil)
+       val value = (NodeSeq.Empty, (_: Env) => Right(a), Nil)
     }
   }
 
@@ -106,17 +133,28 @@ object Formlet {
    */
   def input = {
     new Formlet[String] {
+
       val name = nextFuncName
-      val value =
-        (<input name={{ name }} type="text"/>, (env: Env) => env.apply(name), List(name))
+
+      val func = (env: Env) => {
+        env.get(name).map( Right(_) ).getOrElse( Left("Missing field") )
+      }
+
+      val value = (<input name={{ name }} type="text"/>, func, List(name))
     }
   }
 
   def textarea = {
+
     val name = nextFuncName
+
+    val func = (env: Env) => {
+      env.get(name).map( Right(_) ).getOrElse( Left("Missing field") )
+    }
+
     new Formlet[String] {
       val value =
-        (<textarea name={{ name }}></textarea>, (env: Env) => env.apply(name), List(name))
+        (<textarea name={{ name }}></textarea>, func, List(name))
     }
   }
 
